8f7b2d02db012978ef1ba45bdf7595a31676f9b2
Alisher <dautovalisher33@gmail.com>
2024-07-24 22:15:40 +0500

    Add AuthenticationForm to loginn context

--- user/views.py
+++ user/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def register(request):
     return render(request, 'user/register.html')
 
 def loginn(request):
-    return render(request, 'user/login.html')
+    form = AuthenticationForm()
+
+    context = {'form': form}
+    return render(request, 'user/login.html', context)
