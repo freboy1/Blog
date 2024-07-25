39a8a464a7495b05b7373fdac795c6ae3eb09cee
Alisher <dautovalisher33@gmail.com>
2024-07-25 22:11:13 +0500

    Fix name of register

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='regsiter'),
+    path('register/', views.register, name='register'),
     path('login/', views.loginn, name='login'),
 ]
