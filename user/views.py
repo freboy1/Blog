e3832960a21a3044670233872ffd396c8bd35d00
Alisher <dautovalisher33@gmail.com>
2024-07-25 14:40:38 +0500

    Add for request.POST registers user

--- user/views.py
+++ user/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.urls import reverse
 
 # Create your views here.
 def register(request):
 
     form = UserCreationForm()
     
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+
     context = {'form': form}
 
     return render(request, 'user/register.html', context)
