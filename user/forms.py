73eaea43d795bf324fd467024acef299b63b6420
Alisher <dautovalisher33@gmail.com>
2024-07-25 15:29:46 +0500

    Add model User to RegisterForm

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 class RegisterForm(UserCreationForm):
     class Meta:
         fields = ['username', 'email', 'password1', 'password2']
+        model = User
