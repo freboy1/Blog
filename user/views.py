76c1eda6efc2a3c40b120ad323ac341d7a0ebcfc
Alisher <dautovalisher33@gmail.com>
2024-07-25 18:51:25 +0500

    Add login if request.method 'POST'

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .forms import RegisterForm
-from django.urls import reverse
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def register(request):
@@ -22,6 +22,18 @@ def register(request):
 
 def loginn(request):
     form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
 
     context = {'form': form}
     return render(request, 'user/login.html', context)
