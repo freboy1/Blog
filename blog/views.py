afcc79a08c9da7e30f8c2705345d04b1d0630b0e
Alisher <dautovalisher33@gmail.com>
2024-07-25 22:23:43 +0500

    Add login_required for home view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     return render(request, 'blog/home.html')
