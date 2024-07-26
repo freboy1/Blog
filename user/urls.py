7fc88a60abaefa7b0a3b8df6d66f06dfaf1d1a21
Alisher <dautovalisher33@gmail.com>
2024-07-26 17:25:44 +0500

    Add app_name for user

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name='user'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.loginn, name='login'),
