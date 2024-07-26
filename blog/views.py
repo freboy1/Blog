370c5380ab30234de2644531b2c4f81fb00a8150
Alisher <dautovalisher33@gmail.com>
2024-07-26 22:13:11 +0500

    Add all blogs to context

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Blog
 
 # Create your views here.
 @login_required(login_url='user:login')
 def home(request):
-    return render(request, 'blog/home.html')
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blog/home.html', context)
