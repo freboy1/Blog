eebadc99552fb0d508ed48cf52e8827d4546ef10
Alisher <dautovalisher33@gmail.com>
2024-07-27 19:05:08 +0500

    Add 'blog/add/' url

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/<int:id>/', views.blog, name='blog'),
+    path('blog/add/', views.blog_add, name='blog_add'),
 ]
