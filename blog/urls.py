f85756afb3847b525c9be1d217591bf5bea34e61
Alisher <dautovalisher33@gmail.com>
2024-07-27 22:21:28 +0500

    Add url 'blog/delete/<int:id>/'

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('blog/<int:id>/', views.blog, name='blog'),
     path('blog/add/', views.blog_add, name='blog_add'),
+    path('blog/delete/<int:id>/', views.blog_delete, name='blog_delete'),
 ]
